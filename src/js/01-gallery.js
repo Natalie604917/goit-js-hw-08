0d01a06e57859a91d8780c2c3c047a5d64c0f1d2
Natalie604917 <ni604917@gmail.com>
2022-02-18 17:21:22 +0200

    add task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+
+const cardsContainer = document.querySelector('.gallery');
+const cardsMarkup = createPictureCard(galleryItems);
+
+cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+
+function createPictureCard(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+  })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  
+
+
+
+
+
+
